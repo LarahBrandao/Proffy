c426e27d49cdf8140ebc685b8f5b1a81bd608001
LarahBrandao <larah1108@hotmail.com>
2020-08-07 21:08:59 -0300

    sobe projeto finalizado

---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,53 @@
+import {StyleSheet} from 'react-native';
+import { Poppins_400Regular } from '@expo-google-fonts/poppins';
+import {Archivo_700Bold} from '@expo-google-fonts/archivo';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#8257E5',
+        justifyContent: 'center',
+        padding: 40,
+    },
+
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+
+    title: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 32,
+        lineHeight: 37,
+        maxWidth: 180
+
+    },
+
+    description: {
+        marginTop: 24,
+        color: '#d4c2ff',
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240
+    },
+
+    okButton: {
+        marginVertical: 40,
+        backgroundColor: '#04d361',
+        height: 58,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8
+    },
+
+    okText: {
+        color: '#FFF',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16
+    }
+
+});
+
+export default styles;
---
+++ server/README.md
@@ -0,0 +1,12 @@
+# Funcionalidades
+
+## Conexões
+
+- Rota para  listar o total de conexões realizadas;
+- Rota ara criar uma nova conexão;
+
+## Aulas
+
+- Rota para criar uma aula;
+- Rota para listar aula;
+    - Filtrar por máteria, dia da semana e horário;
